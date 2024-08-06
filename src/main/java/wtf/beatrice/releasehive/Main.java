d184974ee34040c23aaa021e3aa1d31a6015e2a5
Beatrice Dellacà <hello@beatrice.wtf>
2024-08-06 20:01:23 +0200

    start

---
+++ src/main/java/wtf/beatrice/releasehive/Main.java
@@ -0,0 +1,14 @@
+package wtf.beatrice.releasehive;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+
+    }
+}
