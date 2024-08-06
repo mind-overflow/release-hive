61c0216a3ecb659c96e50776bd9debc23f6423e7
Beatrice Dellacà <hello@beatrice.wtf>
2024-08-06 21:48:15 +0200

    RHSRV-1 - implement basic spring boot

--- src/main/java/wtf/beatrice/releasehive/Main.java
+++ src/main/java/wtf/beatrice/releasehive/Main.java
@@ -2,7 +2,10 @@ package wtf.beatrice.releasehive;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
@@ -10,5 +13,8 @@ public class Main {
     public static void main(String[] args) {
         LOGGER.info("Hello world!");
 
+        LOGGER.info("Initializing Spring Boot");
+        SpringApplication.run(Main.class, args);
+        LOGGER.info("Spring Boot initialized!");
     }
 }
