6596c248f126736f2bcaffeab297d2c73db2f6cd
Beatrice Dellacà <hello@beatrice.wtf>
2026-03-12 10:09:42 +0100

    revert: restore ApplicationConfiguration compatible with Spring Security 5 (Spring Boot 3.x)

--- src/main/java/wtf/beatrice/releasehive/config/ApplicationConfiguration.java
+++ src/main/java/wtf/beatrice/releasehive/config/ApplicationConfiguration.java
@@ -40,8 +40,9 @@ public class ApplicationConfiguration
 
     @Bean
     AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
+        authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
